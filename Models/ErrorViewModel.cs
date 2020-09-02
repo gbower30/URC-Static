ee76eba2f58c4f2c782c463e8eb220f4c67430c1
GREG BOWER <greg.bower@utah.edu>
2020-09-02 16:16:20 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace URC_Static.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
